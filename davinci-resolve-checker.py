5b4d25f346c71dc19567facf8259189fec2f3b1d
Andrew Shark <ashark@linuxcomp.ru>
2021-01-17 23:24:49 +0300

    Detecting if intel is primary when I+A

--- davinci-resolve-checker.py
+++ davinci-resolve-checker.py
@@ -5,7 +5,7 @@ import subprocess
 import re
 import pylspci
 
-print("DaVinci Resolve checker", "1.3.2")
+print("DaVinci Resolve checker", "1.4.0")
 
 if distro.id() not in {"arch", "manjaro", "endeavouros"}:
     print("You are running", distro.name(), "(", distro.id(), ") but this script was not tested on it.")
@@ -20,8 +20,7 @@ installed_opencl_drivers = subprocess.getoutput("expac -Qs '%n' opencl-driver").
 from pylspci.parsers import VerboseParser
 lspci_devices = VerboseParser().run()
 
-print("Your configuration:")
-print("Chassis: "+chassis_type)
+print("Chassis type: " + chassis_type)
 print("Installed OpenCL drivers: " + " ".join([str(x) for x in installed_opencl_drivers]))
 
 # Now we are going to check which GPUs are presented in system.
@@ -33,8 +32,14 @@ print("Installed OpenCL drivers: " + " ".join([str(x) for x in installed_opencl_
 # lspci -d ::0380 - amd secondary gpu on an i+a laptop
 
 print("Presented GPUs:")
-print ("\t" + "\n\t".join([ x.device.name + " driver: " + x.driver for x in lspci_devices if x.cls.id in (0x0300, 0x0301, 0x0302, 0x0380) ]))
-print (subprocess.getoutput('glxinfo | grep "OpenGL vendor string"'))
+print ("\t" + "\n\t".join([ x.device.name + " (kernel driver in use: " + x.driver + ")" for x in lspci_devices if x.cls.id in (0x0300, 0x0301, 0x0302, 0x0380) ]))
+
+GL_VENDOR = subprocess.getoutput('glxinfo | grep "OpenGL vendor string" | cut -f2 -d":" | xargs')
+print("OpenGL vendor string: " + GL_VENDOR)
+# By GL_VENDOR we can distinguish not only OpenGL Open/Pro implementations, but also a primary GPU in use (kinda).
+# See https://stackoverflow.com/questions/19985131/how-identify-the-primary-video-card-on-linux-programmatically for more information.
+
+print("")  # Empty line, to separate verdict from configuration info.
 
 found_AMD_GPU = None
 found_INTEL_GPU = None
@@ -46,7 +51,7 @@ for device in lspci_devices:
             if found_INTEL_GPU == None:
                 found_INTEL_GPU = device
             else:
-                print("You have several INTEL GPUs. I am confused. Are you using multi-cpu desktop motherboard?")
+                print("You have several INTEL GPUs. I am confused. Are you using multi-cpu desktop motherboard? Or intel igpu + intel dgpu (which does not exist at the moment of writing)?")
                 exit(1)
         if device.vendor.name == 'Advanced Micro Devices, Inc. [AMD/ATI]':
             if found_AMD_GPU == None:
@@ -73,31 +78,34 @@ if found_AMD_GPU:
         if found_AMD_GPU.driver != 'amdgpu':
             print("You are not using amdgpu as kernel driver. Set radeon.si/cik_support=0 and amdgpu.si/cik_support=1 kernel parameters, otherwise you could not use DaVinci Resolve.")
             exit(1)
-        if subprocess.getoutput('glxinfo | grep "OpenGL vendor string" | cut -f2 -d":" | xargs') != "Advanced Micro Devices, Inc.":
-            print("You are not running AMDGPU-PRO renderer. Install amdgpu-pro-libgl, otherwise you could not use DaVinci Resolve.")
+        if found_INTEL_GPU and GL_VENDOR == "Intel":
+            print("Your primary gpu is Intel. Go to your uefi settings and set primary display to PCIE. Otherwise you could not use DaVinci Resolve (I did not tested it).")
             exit(1)
-        if 'opencl-amdgpu-pro-orca' in installed_opencl_drivers or 'opencl-amdgpu-pro-pal' in installed_opencl_drivers or 'opencl-amd' in installed_opencl_drivers:
-            print("All seems good. You should be able to run DaVinci Resolve successfully.")
-            exit(0)
-        else:
+        if GL_VENDOR != "ATI Technologies Inc.":  # Previously, it was "Advanced Micro Devices, Inc.", but seems it changed.
+            print("You are not using Pro OpenGL implementation. Install amdgpu-pro-libgl, otherwise you could not use DaVinci Resolve.")
+            exit(1)
+        if 'opencl-amd' not in installed_opencl_drivers and 'opencl-amd-polaris' not in installed_opencl_drivers:
             print("You do not have opencl-driver for AMD GPU. Install it, otherwise you could not use DaVinci Resolve.")
             exit(1)
+        else:
+            print("All seems good. You should be able to run DaVinci Resolve successfully.")
+            exit(0)
     elif chassis_type == 'laptop':
         print("I did not found a working configuration with AMD gpu on laptop yet. Did you?")
         exit(1)
 
 if found_NVIDIA_GPU:
-    # I have tested it with D.R. 16.2.3 and found out that BMD have fixed that issue. See https://www.youtube.com/watch?v=NdOGFBHEnkU
+    # I have tested it with DR 16.2.3 and found out that BMD have fixed that issue. See https://www.youtube.com/watch?v=NdOGFBHEnkU
     # So I will keep this comment for a while, then will remove it.
     # if 'opencl-amdgpu-pro-orca' in installed_opencl_drivers or 'opencl-amdgpu-pro-pal' in installed_opencl_drivers or 'opencl-amd' in installed_opencl_drivers:
     #     print("You have installed opencl for amd, but DaVinci Resolve stupidly crashes if it is presented with nvidia gpu. Remove it, otherwise you could not use DaVinci Resolve.")
     if 'opencl-nvidia' not in installed_opencl_drivers:
-        print("You do not have opencl-nvidia package. Install it, otherwise you could not use D.R. Even if you are planning to use cuda, opencl-nvidia will be installed as its dependency.")
+        print("You do not have opencl-nvidia package. Install it, otherwise you could not use DR Even if you are planning to use cuda, opencl-nvidia will be installed as its dependency.")
         exit(1)
     if found_NVIDIA_GPU.driver != 'nvidia':
         print("You are not using nvidia as kernel driver. Use proprietary nvidia driver, otherwise you could not use DaVinci Resolve.")
         exit(1)
-    if subprocess.getoutput('glxinfo | grep "OpenGL vendor string" | cut -f2 -d":" | xargs') != "NVIDIA Corporation":
+    if GL_VENDOR != "NVIDIA Corporation":
         print("You are not running NVIDIA renderer. Try to run the script with prime-run or other method for optimus, otherwise you could not use DaVinci Resolve.")
         exit(1)
     print("All seems good. You should be able to run DaVinci Resolve successfully.")
