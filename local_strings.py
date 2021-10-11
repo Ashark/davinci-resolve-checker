06b6cb79e4648b2f4ad72495cf149e68c499ae6a
Renato Byrro <rmbyrro@gmail.com>
2021-10-11 12:20:20 +0000

    Custom dict to handle localized strings

---
+++ local_strings.py
@@ -0,0 +1,13 @@
+import importlib
+import locale
+from typing import Optional
+
+
+class LocalStrings(dict):
+
+    def __init__(self, preferred_locale: Optional(str) = None) -> None:
+        self.locale: str = preferred_locale or locale.getdefaultlocale()[0]
+        self.local: dict = importlib.import_module(f'languages.{self.locale}').local_str
+
+    def __getitem__(self, key: str) -> str:
+        return self.local[key]
