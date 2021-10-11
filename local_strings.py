2670e42efdd8efd2eee2425f7c597d55f648c6f2
Renato Byrro <rmbyrro@gmail.com>
2021-10-11 12:36:01 +0000

    Fix typo in Optional typing

--- local_strings.py
+++ local_strings.py
@@ -3,10 +3,14 @@ import locale
 from typing import Optional
 
 
+DEFAULT_LOCALE = 'en_US'
+
+
 class LocalStrings(dict):
 
-    def __init__(self, preferred_locale: Optional(str) = None) -> None:
+    def __init__(self, preferred_locale: Optional[str] = None) -> None:
         self.locale: str = preferred_locale or locale.getdefaultlocale()[0]
+
         self.local: dict = importlib.import_module(f'languages.{self.locale}').local_str
 
     def __getitem__(self, key: str) -> str:
