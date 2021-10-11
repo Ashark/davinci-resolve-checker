9fcd75a7c1d938195da3fa5adba4c71249361dd4
Renato Byrro <rmbyrro@gmail.com>
2021-10-11 16:16:05 +0000

    Implement localization in all strings

--- local_strings.py
+++ local_strings.py
@@ -1,6 +1,5 @@
 import importlib
 import locale
-from typing import Optional
 
 
 DEFAULT_LOCALE = 'en_US'
@@ -8,15 +7,15 @@ DEFAULT_LOCALE = 'en_US'
 
 class LocalStrings(dict):
 
-    def __init__(self, preferred_locale: Optional[str] = None) -> None:
-        self.locale: str = preferred_locale or locale.getdefaultlocale()[0]
+    def __init__(self, preferred_locale=None):
+        self.locale = preferred_locale or locale.getdefaultlocale()[0]
 
         try:
-            self.local: dict = importlib.import_module(f'languages.{self.locale}').local_str
+            self.local = importlib.import_module(f'languages.{self.locale}').local_str
         except ModuleNotFoundError:
             print(f'Locale unavailable: {self.locale}, using {DEFAULT_LOCALE} instead')
             self.locale = DEFAULT_LOCALE
             self.local = importlib.import_module(f'languages.{DEFAULT_LOCALE}').local_str
 
-    def __getitem__(self, key: str) -> str:
+    def __getitem__(self, key: str):
         return self.local[key]
