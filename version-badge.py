5af8ead7076db9585e5ed09b801acd4ae6051b4a
Andrew Shark <ashark@linuxcomp.ru>
2023-04-25 18:49:26 +0300

    Use dynamic badge for checker version in readme

---
+++ version-badge.py
@@ -0,0 +1,33 @@
+# This script is used in https://badgen.net/https/ashark.npkn.net/f1a682 to generate version badge.
+# See how it works here: https://css-tricks.com/adding-custom-github-badges-to-your-repo/
+
+from napkin import response  # Note that it is available only in browser in napkin website
+import re
+import requests
+import json
+
+url = "https://raw.githubusercontent.com/Ashark/davinci-resolve-checker/master/davinci-resolve-checker.py"
+file_content = requests.get(url).text
+
+version = "Undetected"
+
+for line in file_content.split('\n'):
+    if "project name" in line:
+        version = line
+        break
+
+# print(version)
+version = re.search(r'"(\d+\.\d+\.\d+.?)"', version).group(1)
+# print(version)
+
+result = """{{
+  "color": "green",
+  "status": "{v}",
+  "subject": "checker version"
+}}""".format(v=version)
+
+# print(result)
+j = json.loads(result)
+
+response.status_code = 200
+response.body = j
